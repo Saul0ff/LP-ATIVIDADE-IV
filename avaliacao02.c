69783824e5614903f7a02509dfd8beb6493e63d6
Saul0ff <143642628+Saul0ff@users.noreply.github.com>
2023-12-13 17:18:23 -0300

    avaliacao02.c

--- avaliacao02.c
+++ avaliacao02.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 struct registro_alunos
 {
@@ -12,7 +13,8 @@ struct registro_alunos
 float calcularMedia(struct registro_alunos *alunos){
     float soma = 0;
     int i, j;
-    for ( i = i; i < 3; i++) {
+    
+    for ( i = 0; i < 3; i++) {
         for ( j = 0; j < 2; j++) {
             soma += alunos[i].notas[j];
         }
@@ -20,18 +22,31 @@ float calcularMedia(struct registro_alunos *alunos){
         soma = 0;
     }
     
-    for (size_t i = 0; i < 3; i++) {
-        printf("%dª aluno: %s", i + 1, alunos[i].nomes);
-        printf("Data Nascimento: %s", alunos[i].dataNascimento);
+    for ( i = 0; i < 3; i++) {
+        printf("%dª aluno: %s\n", i + 1, alunos[i].nomes);
+        printf("Data Nascimento: %s\n", alunos[i].dataNascimento);
         for ( j = 0; j < 2; j++) {
-            printf("%dª nota: %.1f", j + 1, alunos[i].notas[j]);
+            printf("%dª nota: %.1f\n", j + 1, alunos[i].notas[j]);
         }
-        printf("Média: %.1f")
+        printf("Média: %.1f\n", alunos[i].media);
+        aprovadoReprovado(alunos[i]);
     }
     
 }
 
+void aprovadoReprovado(struct registro_alunos alunos){
+    if (alunos.media >= 7)
+    {
+        printf("Aprovado !\n\n");
+    }else{
+        printf("Reprovado !\n\n");
+    }
+}
+
 int main(){
+    
+    setlocale(LC_ALL, "portuguese");
+    
     struct registro_alunos alunos[3];
     int i, j;
     
@@ -44,10 +59,12 @@ int main(){
 
         for ( j = 0; j < 2; j++) {
             printf("Digite %dª nota: ", j + 1);
-            scanf("%f", alunos[i].notas[j]);
-        }    
+            scanf("%f", &alunos[i].notas[j]);
+        }
+        fflush(stdin); 
+        system("cls || clear");   
     }
     
-
+    calcularMedia(alunos);
 
 }
